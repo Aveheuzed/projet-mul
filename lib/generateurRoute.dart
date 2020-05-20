386e9373ad30d51ac7c6e3c0de3f4202e5d79cc9
mounaammar <mounaammar02@gmail.com>
2020-05-20 10:05:40 +0200

    réglage de la classe generateurRoute

--- lib/generateurRoute.dart
+++ lib/generateurRoute.dart
@@ -8,21 +8,22 @@ import 'page2.dart';
 class GenerateurRoute{
   static Route<dynamic> generateRoute(RouteSettings settings){
     final args =settings.arguments;
-    switch(settings.name){
+    final args1=settings.arguments;
+
      /* case '/login' :
        return MaterialPageRoute(builder: (content)=>LoginScreen());
       case '/page2' :
         return MaterialPageRoute(builder: (content)=>Page2Screen());*/
-      case '/exerciceslibres':
+      if(settings.name=='/exerciceslibres')
          return  MaterialPageRoute(builder: (content)=>ExercicesLibres());
-      case '/exercice2':
+
+        if(settings.name=='exercice1')
+        if (args1 is int)
+   return  MaterialPageRoute(builder: (content)=>Exercice1(niveau:args));
+
+        if(settings.name=='exercice1')
         if (args is int)
-   return  MaterialPageRoute(builder: (content)=>Exercice2(nombre:args)
-   );
-      case '/exercice1':
-        if (args is int)
-          return  MaterialPageRoute(builder: (content)=>Exercice1(niveau:args)
-          );
+          return  MaterialPageRoute(builder: (content)=>Exercice2(nombre:args));
   }
 }
 }
